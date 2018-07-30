3725d904435391d9e886ec40ee5d9c5874dcfdf7
Tyler O'Briant <tylerobriant@gmail.com>
2018-07-30 18:09:28 -0500

    try to open up the server to CORs request for React development

--- app.js
+++ app.js
@@ -26,6 +26,12 @@ mongoose.connection
 // setup app
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 routes(app);
 
 module.exports = app;
