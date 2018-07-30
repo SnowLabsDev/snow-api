3f722cac942ee397a1e9cf8728bc610592de8b02
Tyler O'Briant <tylerobriant@gmail.com>
2018-07-30 16:00:30 -0500

    forgot to reinit the port

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const app = require('./app');
 
 // for heroku deployment
-//const PORT = process.env.PORT;
+const PORT = process.env.PORT;
 
-//app.listen(PORT);
+app.listen(PORT);
 
+/*
 // for local testing only
 app.listen(3050, () => {
   console.log('API SERVER');
   console.log('Running on port 3050');
   console.log('CTRL + C to stop');
 });
+*/
